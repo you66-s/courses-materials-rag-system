308d6e9566282845c41ed2efb031b2ac07bdd43b
youssef <youssef@email.com>
2025-12-06 21:56:42 +0100

    first version of the DataLoader

---
+++ backend/data_loader.py
@@ -0,0 +1,12 @@
+from langchain_community.document_loaders import PDFMinerLoader
+
+class DataLoader:
+    def __init__(self, file_path: str, reading_mode: str = "page"):
+        self.__file_path = file_path
+        self.__reading_mode = reading_mode
+        self.__loader = PDFMinerLoader(file_path=self.__file_path, mode=self.__reading_mode)
+
+    def load_data(self):
+        documents = self.__loader.load()
+        return documents
+        
