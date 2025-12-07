d8c6d226fd7b96cfde771f0182adde2544851a80
youssef <youssef@email.com>
2025-12-07 18:15:39 +0100

    change of the pdf parser

--- backend/data_loader.py
+++ backend/data_loader.py
@@ -1,10 +1,10 @@
-from langchain_community.document_loaders import PDFMinerLoader
+from langchain_community.document_loaders import PyMuPDFLoader
 
 class DataLoader:
     def __init__(self, file_path: str, reading_mode: str = "page"):
         self.__file_path = file_path
         self.__reading_mode = reading_mode
-        self.__loader = PDFMinerLoader(file_path=self.__file_path, mode=self.__reading_mode)
+        self.__loader = PyMuPDFLoader(file_path=self.__file_path, mode=self.__reading_mode)
 
     def load_data(self):
         documents = self.__loader.load()
