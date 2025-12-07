816c982246931cab67ed674d966ee94e377e7a26
youssef <youssef@email.com>
2025-12-07 18:15:06 +0100

    amelioration of the chunker

--- backend/chunker.py
+++ backend/chunker.py
@@ -2,11 +2,11 @@ from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 class Chunker:
-    def __init__(self, chunk_size: int = 1000, chunk_overlap: int = 200):
+    def __init__(self, chunk_size: int = 500, chunk_overlap: int = 75):
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
 
-    def chunk_documents(self, documents: Document):
+    def chunk_documents(self, document: Document):
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap, length_function=len, separators=["\n\n", "\n", " ", ""])
-        chunks = text_splitter.split_documents(documents)
+        chunks = text_splitter.split_documents(document)
         return chunks
