0781f0909475c94f6e8ab9e533dd97ff354081fc
youssef <youssef@email.com>
2025-12-08 15:32:09 +0100

    problem of 0 document returned resolved

---
+++ backend/retriever.py
@@ -0,0 +1,28 @@
+from embeddings_model import EmbeddingsModel
+from vectorDB import VectorDataBase
+
+class Retriever:
+    def __init__(self, collection_name: str = "course_materials"):
+        self.__vector_db = VectorDataBase(collection_name=collection_name)
+        self.__embedding_model = EmbeddingsModel()
+        
+    def retrieve(self, query: str, top_k: int, distance_threshold: float = 0.65) -> list[dict]:
+        retrieved_docs = []
+        try:
+            query_embedding = self.__embedding_model.embed_texts([query])[0]
+            results = self.__vector_db.query_db(query_embedding=query_embedding, top_k=top_k)
+            documents = results['documents'][0]
+            distances = results['distances'][0]
+            metadata = results['metadatas'][0]
+            for i, (doc, dist, meta) in enumerate(zip(documents, distances, metadata)):
+                if dist > distance_threshold:
+                    retrieved_docs.append({
+                        "document": doc,
+                        "metadata": meta
+                    })
+            return retrieved_docs
+        except Exception as err:
+            print("Error retrieving documents:", err)
+            return None
+
+        
